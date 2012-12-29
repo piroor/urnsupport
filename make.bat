138a094efdb868964a4a2bb28aae4d1f46680008
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2012-12-29 19:50:52 +0900

    Create XPI without jar file

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 copy buildscript\makexpi.sh .\
-bash makexpi.sh urnsupport version=0
+bash makexpi.sh -n urnsupport -o
 del makexpi.sh
--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@
 appname=urnsupport
 
 cp buildscript/makexpi.sh ./
-./makexpi.sh $appname version=0
+./makexpi.sh -n $appname -o
 rm ./makexpi.sh
 
